4034378d51dc99507a15cd5b9165b095e3132fcb
Brandon Hawkins <bhawkins6177@gmail.com>
2021-06-19 11:18:57 -0400

    Added the operate function

--- script.js
+++ script.js
@@ -10,3 +10,22 @@ function mult (num1, num2) {
 function div (num1, num2) {
     return num1/num2;
 }
+
+function operate (num1, num2, operator){
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-'){
+        return sub(num1, num2);
+    } else if (operator === '*'){
+        return mult(num1, num2); 
+    } else if (operator ==='/'){
+        return div(num1, num2);
+    }else {
+        return 'an error has occured';
+    }
+        
+    }
+
+console.log (operate(50,2,'*'))
+
+
